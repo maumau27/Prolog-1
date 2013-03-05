9b2083d54676e0c21d6580cba257f5834a2a5713
Spenser Steele <spenser.steele@gmail.com>
2013-03-05 14:20:10 -0600

    more efficient

--- dunsmore.pl
+++ dunsmore.pl
@@ -1,17 +1,28 @@
-%jobs
+% jobs
 sold(mrmason,poison).
 
-%relationships
+% relationships
+% X is married to Y
 married(lorddunsmore,ladyemily).
 married(gardner,maid).
+
+% X is a rival of Y
 rival(lorddunsmore,lordsecrest).
-customer(inspector,mrmason).
+
+% X is a daughter of Y
 daughter(misssecrest,lordsecrest).
+
+% X is a customer of Y
+customer(inspector,mrmason).
 customer(lordsecrest,mrmason).
+
+% X is a suitor of Y
 suitor(professor,ladyemily).
+
+% X is in debt to Y
 debt(butler,lorddunsmore).
 
-%hobbies
+% X has hobby Y
 hobby(professor,philosopher).
 hobby(ladyemily,music).
 hobby(mrmason,philosophy).
@@ -21,21 +32,29 @@ hobby(gardener,music).
 
 % X is friends with Y if X and Y share hobby Z
 friends(X,Y):- hobby(X,Z),hobby(Y,Z).
+relations(X,Y):- married(X,Y).
+relations(X,Y):- married(Y,X).
+relations(X,Y):- daughter(X,Y).
+relations(X,Y):- daughter(Y,X).
+relations(X,Y):- suitor(X,Y).
+relations(X,Y):- suitor(Y,X).
 
 % X has the ability to kill if X is a customer of Y who sold a weapon
 weapon(X):- customer(X,Y),sold(Y,poison).
 
 % X has access to a weapon if X is friends with Y who has a weapon
-weaponaccess(X):- friends(X,Y),weapon(Y).
-weaponaccess(X):- married(X,Y),weapon(Y).
-weaponaccess(X):- married(Y,X),weapon(Y).
-weaponaccess(X):- daughter(X,Y),weapon(Y).
-weaponaccess(X):- daughter(Y,X),weapon(Y).
-weaponaccess(X):- suitor(X,Y),weapon(Y).
-weaponaccess(X):- suitor(Y,X),weapon(Y).
-
+means(X):- weapon(X).
+means(X):- friends(X,Y),weapon(Y).
+means(X):- relations(X,Y),weapon(Y).
 
 % X has motive to kill Y
-motive(X,Y):- rival(Y,X),weapon(X).
-motive(X,Y):- married(Y,X),weapon(Y).
-motive(X,Y):- debt(X,Y),weapon(X).
+motive(X,Y):- rival(X,Y).
+motive(X,Y):- rival(Y,X).
+motive(X,Y):- relations(X,Y).
+motive(X,Y):- relations(Y,X).
+motive(X,Y):- debt(X,Y).
+motive(X,Y):- debt(Y,X).
+
+
+% X killed Y if they have motive
+killed(X,Y):- motive(X,Y), means(X).
